1cc1a7080f416f7064b522b1dc9104392ba8e42c
SOHAM-THUMMAR <sohamthummar04@gmail.com>
2026-02-11 07:11:04 +0530

    exception is tested and fixed some errors

--- src/exception.py
+++ src/exception.py
@@ -1,20 +1,23 @@
 import sys
+import logging
 
-def error_message_detail(error, error_detail: sys):
-    
-    _, _, exc_tb = error_detail.exc_info()
+
+def error_message_detail(error):
+    _, _, exc_tb = sys.exc_info()
+
+    if exc_tb is None:
+        return str(error)
 
     file_name = exc_tb.tb_frame.f_code.co_filename
     line_number = exc_tb.tb_lineno
-    error = str(error)
 
     return f"Error in {file_name}, line {line_number}: {error}"
 
 
 class CustomException(Exception):
-    def __init__(self, error, error_detail: sys):
-        super().__init__(error)
-        self.error_message = error_message_detail(error, error_detail)
+    def __init__(self, error):
+        super().__init__(str(error))
+        self.error_message = error_message_detail(error)
 
     def __str__(self):
         return self.error_message
